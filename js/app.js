9ffc559e82ea14a487cb73cf14ff453ee6741bc4
Andrey Ziuzin <zyzin@hotmail.com>
2021-02-11 22:25:24 +0300

    Main block now receiving data from api

--- js/app.js
+++ js/app.js
@@ -1,38 +1,61 @@
+const apiKey = '56b7d91218774cf2f9808498488f31f9';
 const asideMainBox = document.querySelector('.aside__info');
 const footerDate = document.querySelector('.aside__footer-day');
 const footerLocarion = document.querySelector('.aside__footer-location');
-
+const humidityBox = document.querySelector('.main-bar__data--humidity');
+const windSpeed = document.querySelector('.main-bar__data--speed');
+const range = document.querySelector('.main-bar__range');
+const windIcon = document.querySelector('.main-bar__wind-icon');
+const visability = document.querySelector('.main-bar__data--visability');
+const pressureBox = document.querySelector('.main-bar__data--pressure');
 const date = `${new Date()}`;
 const currDay = date.split(' ')[0];
 const currDate = date.split(' ')[2];
 const currMonth = date.split(' ')[1];
 
-const cityCinput = 'barcelona';
+const cityInput = 'rome';
 
 async function getData(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=56b7d91218774cf2f9808498488f31f9&units=metric`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
     );
+
+    if (!response.ok) throw new Error('Problem getting City');
+
+    const data = await response.json();
+    renderMainInfo(data);
+  } catch (err) {
+    console.log(`Yo, your error: ${err.message}`);
+  }
+}
+
+async function getForecast(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`,
+    );
+
+    if (!response.ok) throw new Error('Problem getting City');
     const data = await response.json();
     console.log(data);
-    renderMainInfo(data);
   } catch (err) {
     console.log(err.message);
   }
 }
 
 function renderMainInfo(data) {
-  const { temp: currentTemp } = data.main;
+  const { temp: currentTemp, humidity, pressure } = data.main;
+  const { speed, deg } = data.wind;
   const { main: currentWeather, icon: imgId } = data.weather[0];
 
-  mainHtml = `
+  const mainHtml = `
           <li class="aside__info-item">
             <img class="aside__info-img" src="https://openweathermap.org/img/wn/${imgId}@2x.png" alt="" />
           </li>
           <li class="aside__info-item"
             ><p class="aside__info-data"
-              >${Math.trunc(
+              >${Math.round(
                 currentTemp,
               )}<span class="temper-type temper-type--big">C</span></p
             ></li
@@ -41,22 +64,25 @@ function renderMainInfo(data) {
             ><p class="aside__info-decription">${currentWeather}</p>
           </li>
   `;
+
   asideMainBox.insertAdjacentHTML('beforeend', mainHtml);
   footerDate.textContent = `${currDay}, ${currDate} ${currMonth}`;
-
-  footerLocarion.textContent = `${cityCinput[0].toUpperCase()}${cityCinput.slice(
+  footerLocarion.textContent = `${cityInput[0].toUpperCase()}${cityInput.slice(
     1,
   )}`;
+
+  humidityBox.textContent = `${humidity}`;
+  range.value = humidity;
+  windSpeed.textContent = Math.round(speed);
+  windIcon.style.transform = `rotate(${deg - 45}deg)`;
+  visability.textContent = `${(data.visibility / 1000).toFixed(1)}`;
+  pressureBox.textContent = pressure;
 }
+
+function renderForecast(data) {}
 
 function updateUI() {
-  getData(cityCinput);
+  getData(cityInput);
+  getForecast(cityInput);
 }
 updateUI();
-
-// const {
-//   temp: currentTemp,
-//   temp_min: minTemp,
-//   temp_max: maxTemp,
-//   pressure,
-// } = data.main;
