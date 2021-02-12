b3adeafab6725064d634f3756df4015f28d9525f
Andrey Ziuzin <zyzin@hotmail.com>
2021-02-12 12:48:11 +0300

    forecast section is ready

--- js/app.js
+++ js/app.js
@@ -8,22 +8,24 @@ const range = document.querySelector('.main-bar__range');
 const windIcon = document.querySelector('.main-bar__wind-icon');
 const visability = document.querySelector('.main-bar__data--visability');
 const pressureBox = document.querySelector('.main-bar__data--pressure');
+const forecastBox = document.querySelector('.top-bar__forecast');
 const date = `${new Date()}`;
 const currDay = date.split(' ')[0];
-const currDate = date.split(' ')[2];
+const currDate = +date.split(' ')[2];
 const currMonth = date.split(' ')[1];
-
-const cityInput = 'rome';
+// metric
+const cityInput = 'moscow';
 
 async function getData(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`,
     );
 
     if (!response.ok) throw new Error('Problem getting City');
 
     const data = await response.json();
+    console.log(data);
     renderMainInfo(data);
   } catch (err) {
     console.log(`Yo, your error: ${err.message}`);
@@ -33,12 +35,12 @@ async function getData(city) {
 async function getForecast(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`,
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`,
     );
 
     if (!response.ok) throw new Error('Problem getting City');
     const data = await response.json();
-    console.log(data);
+    renderForecast(data);
   } catch (err) {
     console.log(err.message);
   }
@@ -79,7 +81,46 @@ function renderMainInfo(data) {
   pressureBox.textContent = pressure;
 }
 
-function renderForecast(data) {}
+function renderForecast(data) {
+  const { list } = data;
+  const newList = list
+    .filter((el) => {
+      const newtime = el.dt_txt.split(' ')[1];
+      if (newtime === '12:00:00') return el;
+    })
+    .map((el) => {
+      const dateFull = `${new Date(el.dt_txt)}`.split(' ');
+      const day = dateFull[0];
+      const month = dateFull[1];
+      const date = +dateFull[2];
+      const { icon } = el.weather[0];
+      const { temp_max: maxTemp, temp_min: minTemp } = el.main;
+
+      let insertDate = `${day}, ${date} ${month}`;
+      if (currDate === date) {
+        insertDate = 'Today';
+      }
+      if (date === currDate + 1) {
+        insertDate = 'Tomorrow';
+      }
+      html = `
+      <li class="top-bar__forecast-card">
+                <h5 class="top-bar__date">${insertDate}</h5>
+                <img src="https://openweathermap.org/img/wn/${icon}.png" alt="" class="top-bar__img" />
+                <div class="top-bar__temp">
+                  <p class="top-bar__day">${Math.round(
+                    maxTemp,
+                  )}<span class="temp">C</span></p>
+                  <p class="top-bar__day--min">${Math.round(
+                    minTemp,
+                  )}<span class="temp">C</span></p>
+                </div>
+              </li>
+      `;
+
+      forecastBox.insertAdjacentHTML('beforeend', html);
+    });
+}
 
 function updateUI() {
   getData(cityInput);
